bed0d09bf7a176d00b6f86f63006d8f56e6ba183
Joao Iguana <jiboiadamor@gmail.com>
2024-03-12 11:46:45 +0000

    practiced string basic operations

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+first_name = "João"
+last_name = "Garcia"
+full_name = first_name + " " + last_name
+print(full_name)
+
+repeat_str = "Cavaco!" * 3
+print(repeat_str)
+
+text = "Adoro iguanas!"
+print(text[:5])
+print(text[6:])
+
+print(first_name[:2], last_name * 3)
